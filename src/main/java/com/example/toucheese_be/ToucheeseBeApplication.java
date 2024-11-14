45d20881c48bcd1b38abb1130aef608ef12ac41c
JeUk Park <77144006+sleo113@users.noreply.github.com>
2024-11-14 20:17:38 +0900

    Settings - 1

---
+++ src/main/java/com/example/toucheese_be/ToucheeseBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.toucheese_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToucheeseBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToucheeseBeApplication.class, args);
+	}
+
+}
