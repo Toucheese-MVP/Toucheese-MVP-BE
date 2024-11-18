13ebcd925935313358450bed86b240285fbce809
JeUk Park <77144006+sleo113@users.noreply.github.com>
2024-11-18 15:44:55 +0900

    Modify Studio.java

--- src/main/java/com/example/toucheese_be/domain/studio/entity/Studio.java
+++ src/main/java/com/example/toucheese_be/domain/studio/entity/Studio.java
@@ -1,9 +1,7 @@
 package com.example.toucheese_be.domain.studio.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
 import java.time.LocalDateTime;
 import java.util.List;
 import lombok.AllArgsConstructor;
@@ -17,19 +15,33 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Studio {
+
+    // 컨셉 아이디
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(length=150)
     private Long id;
 
+    //스튜디오 이름
+    @Column(length=150)
     private String name;
 
+    // 스튜디오 프로필 사진
+    @Column(length=150)
     private String profileImg;
 
+    // 스튜디오에서 예시로 제공하는 대표사진
+    @Column(length=150)
     private String portfolioImg;
 
-    private Double avgRating;
+    // 스튜디오별 평균 별점
+    // asterion = 별점
+    @Column(length=150)
+    private Double avgAsterion;
 
+    // 실제 스튜디오 위치 주소
+    @Column(length=150)
     private String address;
-    // 상품 리스트
-    // 컨셉 아이디
+
+    // private int price;
 }
