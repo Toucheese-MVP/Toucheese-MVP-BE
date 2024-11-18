408844d076006a95e2a0237b85f0f67fc10ef36c
m1nddoong <alstjsdlr0321@korea.ac.kr>
2024-11-18 13:39:37 +0900

    feat/ET-48 : 스튜디오 엔티티 수정

--- src/main/java/com/example/toucheese_be/studio/entity/Studio.java
+++ src/main/java/com/example/toucheese_be/studio/entity/Studio.java
@@ -5,12 +5,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import java.time.LocalDateTime;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
+@Setter
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
@@ -18,26 +20,11 @@ public class Studio {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Setter
     private String name;
-    @Setter
-    private String imageUrl;
-    @Setter
-    private String email;
-    @Setter
-    private String phone;
-    @Setter
-    private String instaUrl;
-    @Setter
-    private String region;
-    @Setter
+    private String profileImg;
+    private String portfolioImg;
+    private Double avgRating;
     private String address;
-    @Setter
-    private Integer price;
-    @Setter
-    private String schedule;
-    @Setter
-    private String announcement;
-    @Setter
-    private String notes;
+    // 상품 리스트
+    // 컨셉 아이디
 }
