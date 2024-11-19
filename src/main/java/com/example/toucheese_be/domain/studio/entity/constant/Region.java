954de486159b6d7f8ab86ddbaec4eff8be392999
m1nddoong <alstjsdlr0321@korea.ac.kr>
2024-11-19 14:12:47 +0900

    feat: Concept 항목 추가

--- src/main/java/com/example/toucheese_be/domain/studio/entity/constant/Region.java
+++ src/main/java/com/example/toucheese_be/domain/studio/entity/constant/Region.java
@@ -7,9 +7,13 @@ import lombok.Getter;
 public enum Region {
     ALL("전체"),
     GANGNAM("강남구"),
-
-    GANGSEO("강서구"),
+    SEOCHO("서초구"),
+    SONGPA("송파구"),
+    MAPO("마포구"),
+    YEONGDEUNGPO("영등포구"),
     GANGBUK("강북구"),
+    YONGSAN("용산구"),
+    SEONGDONG("성동구");
 
     private final String description;
 
