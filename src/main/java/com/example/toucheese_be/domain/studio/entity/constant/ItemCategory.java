96fd6fd9566193fedd7af915b7f7685c59092a59
m1nddoong <alstjsdlr0321@korea.ac.kr>
2024-11-21 14:09:52 +0900

    feat: Item 엔티티에 카테고리 추가

---
+++ src/main/java/com/example/toucheese_be/domain/studio/entity/constant/ItemCategory.java
@@ -0,0 +1,16 @@
+package com.example.toucheese_be.domain.studio.entity.constant;
+
+
+import lombok.Getter;
+
+@Getter
+public enum ItemCategory {
+    PROFILE_PHOTO("프로필사진"),
+    ID_PHOTO("증명사진");
+
+    private final String description;
+
+    ItemCategory(String description) {
+        this.description = description;
+    }
+}
