89aa59037ac7e77c44ff9bc8b317626d400dfbf1
m1nddoong <alstjsdlr0321@korea.ac.kr>
2024-11-22 13:36:25 +0900

    feat: StudioSearchFilterDto 에 Pagable 정보 통합

--- src/main/java/com/example/toucheese_be/domain/studio/repository/StudioRepositoryImpl.java
+++ src/main/java/com/example/toucheese_be/domain/studio/repository/StudioRepositoryImpl.java
@@ -30,30 +30,30 @@ public class StudioRepositoryImpl implements StudioRepositoryCustom {
     @Override
     public Page<StudioDto> getStudioListWithPages(
             Long conceptId,
-            StudioSearchFilterDto filters,
+            StudioSearchFilterDto dto,
             Pageable pageable
     ) {
         // 기본 조건 : 컨셉 ID 로 필터링
         BooleanBuilder builder = new BooleanBuilder();
         builder.and(studio.concept.id.eq(conceptId));
 
-        if (filters != null) {
+        if (dto != null) {
             // 지역 필터
-            if (filters.getRegion() != null && filters.getRegion() != Region.ALL) {
-                builder.and(studio.address.containsIgnoreCase(filters.getRegion().getDescription()));
+            if (dto.getRegion() != null && dto.getRegion() != Region.ALL) {
+                builder.and(studio.address.containsIgnoreCase(dto.getRegion().getDescription()));
             }
             // 인기 필터
-            if (filters.getPopularity() != null && filters.getPopularity() != Popularity.ALL) {
-                builder.and(studio.popularity.goe(filters.getPopularity().getMinRating()));
+            if (dto.getPopularity() != null && dto.getPopularity() != Popularity.ALL) {
+                builder.and(studio.popularity.goe(dto.getPopularity().getMinRating()));
             }
 
             // 가격 필터: 가장 저렴한 Item 가격 기준
-            if (filters.getPriceFilter() != null && filters.getPriceFilter() != PriceFilter.ALL) {
+            if (dto.getPriceFilter() != null && dto.getPriceFilter() != PriceFilter.ALL) {
                 builder.and(
                         JPAExpressions.select(item.price.min())
                                 .from(item)
                                 .where(item.studio.eq(studio))
-                                .loe(filters.getPriceFilter().getMaxPrice())
+                                .loe(dto.getPriceFilter().getMaxPrice())
                 );
             }
         }
