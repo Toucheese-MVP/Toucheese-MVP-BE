9d06a4a610371127b6f8e828986675ce69aafd81
m1nddoong <alstjsdlr0321@korea.ac.kr>
2024-11-27 02:14:21 +0900

    feat: StudioDutyDate 휴무일만 저장하도록 엔티티 수정

--- src/main/java/com/example/toucheese_be/domain/studio/entity/StudioDutyDate.java
+++ src/main/java/com/example/toucheese_be/domain/studio/entity/StudioDutyDate.java
@@ -1,11 +1,6 @@
 package com.example.toucheese_be.domain.studio.entity;
 
-import com.example.toucheese_be.domain.studio.entity.constant.DayType;
-import com.example.toucheese_be.domain.studio.entity.constant.DutyType;
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -32,19 +27,13 @@ public class StudioDutyDate {
     @JoinColumn(name = "studio_id", nullable = false)
     private Studio studio;
 
-    // 근무 시간 또는 휴무일 여부
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
-    private DutyType type;
-
-    // 근무 시간의 요일 (주 단위)
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
-    private DayType day;
-
-    // 근무 시작 시간 (타입: time)
+    // 근무 시간
     private LocalTime openTime;
-
-    // 근무 종료 시간 (타입: time)
     private LocalTime closeTime;
+    // 휴무 시간
+    private LocalTime breakStartTime;
+    private LocalTime breakEndTime;
+
+    // 휴무일만 저장 ("SATURDAY,SUNDAY")
+    private String holidays;
 }
