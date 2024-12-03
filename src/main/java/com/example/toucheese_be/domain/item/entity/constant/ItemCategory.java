024a1bbbef94813adc6580f77629e67ea5955544
m1nddoong <alstjsdlr0321@korea.ac.kr>
2024-12-03 14:35:33 +0900

    feat: 아이템 카테고리 (세미화보) 추가

--- src/main/java/com/example/toucheese_be/domain/item/entity/constant/ItemCategory.java
+++ src/main/java/com/example/toucheese_be/domain/item/entity/constant/ItemCategory.java
@@ -8,8 +8,10 @@ public enum ItemCategory {
     PROFILE_PHOTO("프로필사진"),
     ID_PHOTO("증명사진"),
     PORTRAIT_PHOTO("화보사진"),
+    SEMI_PORTRAIT_PHOTO("세미화보"),
     GROUP_PHOTO("단체사진"),
     UNKNOWN("촬영상품");
+
 
     private final String description;
 
