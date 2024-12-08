28c7589f0b0ab120922732ae29187e9ea82738bc
JeUk Park <77144006+sleo113@users.noreply.github.com>
2024-12-08 16:14:59 +0900

    feat: 서비스 파일 구현, 컨트롤러 파일 구현, DTO 파일 구현

--- src/main/java/com/example/toucheese_be/domain/auth/user/dto/UserDto.java
+++ src/main/java/com/example/toucheese_be/domain/auth/user/dto/UserDto.java
@@ -1,4 +1,22 @@
-//package com.example.toucheese_be.domain.user.dto;
+//package com.example.toucheese_be.domain.auth.user.dto;
 //
+//import com.example.toucheese_be.domain.auth.user.entity.User;
+//import com.example.toucheese_be.domain.order.dto.OrderUserDto;
+//import lombok.AllArgsConstructor;
+//import lombok.Builder;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//
+//@Getter
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Builder
 //public class UserDto {
+//    private Long id;
+//    private String name;
+//    private String profileImg;
+//    private String phoneNumber;
+//    private String email;
+//
+//
 //}
--- src/main/java/com/example/toucheese_be/domain/order/dto/OrderDetailDto.java
+++ src/main/java/com/example/toucheese_be/domain/order/dto/OrderDetailDto.java
@@ -1,21 +1,40 @@
 package com.example.toucheese_be.domain.order.dto;
 
+import com.example.toucheese_be.domain.order.entity.Order;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+@Builder
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class OrderDetailDto {
     // 주문 ID
     private Long orderId;
     // 내 정보
-    private OrderUserDto OrderUserDto;
+    private OrderUserDto orderUserDto;
     // 스튜디오 이름
     private String studioName;
     // 예약 시간
     private String reservedDateTime;
     // 상품 확인
-    private OrderItemDto OrderItemDto;
+    private List<OrderItemDto> orderItemDto;
+
+    public static OrderDetailDto fromEntity(Order entity) {
+        return OrderDetailDto.builder()
+                .orderId(entity.getId())
+                .orderUserDto(OrderUserDto.fromEntity(entity.getUser())) // User 객체에서 OrderUserDto로 변환
+                .studioName(entity.getStudio().getName())
+                .reservedDateTime(entity.getOrderDateTime().toString()) // 필요에 따라 형식 변환
+                .orderItemDto(entity.getOrderItems().stream()
+                        .map(OrderItemDto::fromEntity) // OrderItemDto 변환 필요
+                        .collect(Collectors.toList()))
+                .build();
+
+    }
 }
