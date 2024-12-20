596957ad8da57ce99558fe4b8046b6bcd51dee79
JeUk Park <77144006+sleo113@users.noreply.github.com>
2024-12-20 13:57:19 +0900

    feat: 예외 처리 && 예약 취소 Controller && Service 작성 완료

---
+++ src/main/java/com/example/toucheese_be/global/exception/AlreadyCancelledException.java
@@ -0,0 +1,7 @@
+package com.example.toucheese_be.global.exception;
+
+public class AlreadyCancelledException extends RuntimeException{
+    public AlreadyCancelledException(Long orderId){
+        super("이미 취소된 주문입니다." + orderId);
+    }
+}
