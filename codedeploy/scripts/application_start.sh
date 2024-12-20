918915394d2be1ff15b3265e0684f4326b6ba8bc
m1nddoong <alstjsdlr0321@korea.ac.kr>
2024-12-20 23:03:53 +0900

    feat: CD 구성 - 5

--- codedeploy/scripts/application_start.sh
+++ codedeploy/scripts/application_start.sh
@@ -7,11 +7,8 @@ if [ $running -eq 1 ]; then
   sudo docker stop todo-cd
 fi
 
-# S3에서 .env 파일 다운로드
-aws s3 cp s3://toucheese-team2-s3/env env
-
 # 최신 이미지를 pull
 sudo docker pull toucheeseteam2/toucheese:latest
 
-# 컨테이너 실행 (다운로드한 env 파일을 --env-file로 사용)
-sudo docker run --env-file env -d --name toucheese-cd -p 8080:8080 toucheeseteam2/toucheese:latest
+# 컨테이너 실행 (다운로드한 .env 파일을 --env-file로 사용)
+sudo docker run --env-file /home/ubuntu/.env -d --name toucheese-cd -p 8080:8080 toucheeseteam2/toucheese:latest
