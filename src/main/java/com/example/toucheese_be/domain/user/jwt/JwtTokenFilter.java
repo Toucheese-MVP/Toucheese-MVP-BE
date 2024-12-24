d511c13ff96ae65ac5b6b5c85ba1490a196ac591
m1nddoong <alstjsdlr0321@korea.ac.kr>
2024-12-24 18:50:40 +0900

    refactor: Swagger 적용 URL 수정

--- src/main/java/com/example/toucheese_be/domain/user/jwt/JwtTokenFilter.java
+++ src/main/java/com/example/toucheese_be/domain/user/jwt/JwtTokenFilter.java
@@ -30,13 +30,11 @@ public class JwtTokenFilter extends OncePerRequestFilter {
     @Override
     protected boolean shouldNotFilter(HttpServletRequest request) {
         String[] excludePath = {
-                "/swagger-ui/",
-                "/swagger-ui/**",
-                "/v3/api-docs/",
                 "/v3/api-docs/**",
-                "/static/**",
-                "/resources/**",
-                "/META-INF/resources/**",
+                "/swagger-ui/**",
+                "/swagger-resources/**",
+                "/webjars/**",
+                "/swagger-ui.html",
                 "/public/**",
                 "/api/v1/studio/**",
                 "/api/v1/auth/sign-up/**",
