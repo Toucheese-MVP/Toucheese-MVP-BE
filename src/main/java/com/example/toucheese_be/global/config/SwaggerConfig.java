9399db77e0a0b6bd00061ca4f2de4d1235d506f8
민선익 <minseonik@minseon-ig-ui-MacBookPro.local>
2025-01-07 17:27:28 +0900

    feat: Swagger Settings 2

--- src/main/java/com/example/toucheese_be/global/config/SwaggerConfig.java
+++ src/main/java/com/example/toucheese_be/global/config/SwaggerConfig.java
@@ -1,39 +1,41 @@
 package com.example.toucheese_be.global.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Toucheese API",
+                description = "toucheese api"
+        )
+)
 public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
-        // API 기본 정보 설정
-        Info info = new Info()
-                .title("Toucheese API")
-                .version("1.0")
-                .description("터치즈 API 목록입니다.");
+        SecurityScheme securityScheme = getSecurityScheme();
+        SecurityRequirement securityRequirement = getSecurityRequireMent();
 
-        // Security Scheme 정의
-        SecurityScheme securityScheme = new SecurityScheme()
-                .type(SecurityScheme.Type.HTTP)
-                .scheme("bearer")
-                .bearerFormat("JWT")
-                .in(SecurityScheme.In.HEADER)
-                .name("Authorization");
-
-        // Security Requirement 정의
-        SecurityRequirement securityRequirement = new SecurityRequirement().addList("BearerAuth");
-        // Swagger UI 설정 및 보안 추가
         return new OpenAPI()
-                .addServersItem(new Server().url("http://localhost:8080"))
-                .addServersItem(new Server().url("http://3.39.13.219"))
-                .info(info)
-                .addSecurityItem(securityRequirement)
-                .schemaRequirement("BearerAuth", securityScheme);
+                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
+                .security(List.of(securityRequirement));
+    }
+
+    private SecurityScheme getSecurityScheme() {
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER).name("Authorization");
+    }
+
+    private SecurityRequirement getSecurityRequireMent() {
+        return new SecurityRequirement().addList("bearerAuth");
     }
 }
